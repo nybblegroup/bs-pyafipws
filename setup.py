78871441cbefc3bc19c32962d0dc9b8b157feaa7
capa0soluciones <capa0soluciones@gmail.com>
2025-07-03 12:45:09 -0300

    fix on setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="bs-pyafipws",  # CAMBIADO para evitar conflicto con el original
-    version="1.0.0-nybble",  # Usá lo que corresponda, o podés fijar un commit hash
-    description="Fork de PyAfipWs adaptado por NybbleGroup",
-    long_description="Versión personalizada del cliente AFIP WS de Reingart, mantenida por NybbleGroup",
-    author="NybbleGroup",
-    author_email="info@nybblegroup.com",
-    url="https://github.com/nybblegroup/bs-pyafipws",
+    name="bs-pyafipws",  # Nuevo nombre para evitar conflictos
+    version="3.6.0-nybble",  # Version personalizada para V11
+    description="Fork del cliente AFIP de Reingart, mantenido por NybbleGroup",
+    long_description="Versión personalizada del paquete PyAfipWs, con cambios específicos aplicados por NybbleGroup.",
+    author="Mariano Reingart",
+    author_email="reingart@gmail.com",
+    license="GNU GPL v3+",
+    url="https://github.com/nybblegroup/bs-pyafipws",  # O tu fork si querés
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Natural Language :: Spanish"
-    ],
     python_requires='>=3.6',
     include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.6",
+        "Natural Language :: Spanish",
+    ],
 )
